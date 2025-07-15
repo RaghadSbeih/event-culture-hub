7c8997e3d726f93ecea773d37cf4e6c23b23cdbc
Raghad Sbeih <148389472+RaghadSbeih@users.noreply.github.com>
2025-07-16 01:01:09 +0300

    feat: add Palestinian cities selection to event creation form (#22)

--- culture_hub/views.py
+++ culture_hub/views.py
@@ -290,6 +290,22 @@ def create_event(request):
         "Conferences & Tech",
         "Other"
     ]
+    palestinian_cities = [
+        "Jerusalem (Al-Quds)",
+        "Ramallah",
+        "Gaza",
+        "Hebron (Al-Khalil)",
+        "Nablus",
+        "Bethlehem",
+        "Jericho",
+        "Jenin",
+        "Tulkarm",
+        "Qalqilya",
+        "Haifa",
+        "Jaffa (Yafa)",
+        "Acre (Akka)",
+        "Nazareth (Al-Nasirah)"
+    ]
     from datetime import date, datetime
     if request.method == 'POST':
         user = User.objects.get(id=request.session['user_id'])
@@ -356,7 +372,7 @@ def create_event(request):
     categories = Category.objects.all()
     if not categories.exists():
         categories = [{'id': i+1, 'name': name} for i, name in enumerate(default_categories)]
-    return render(request, 'create_event.html', {'categories': categories, 'errors': errors, 'form_data': form_data})
+    return render(request, 'create_event.html', {'categories': categories, 'cities': palestinian_cities, 'errors': errors, 'form_data': form_data})
 
 @login_required
 def organizer_manage_bookings(request):
