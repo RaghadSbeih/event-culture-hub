db3ba6138128817ccd16885890fe19f307da3a6e
Raghad Sbeih <148389472+RaghadSbeih@users.noreply.github.com>
2025-07-16 01:44:17 +0300

    feat: add event detail view and enhance create event form (#23)

--- culture_hub/urls.py
+++ culture_hub/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('event/create/', views.create_event, name='create_event'),
     path('event/<int:event_id>/edit/', views.edit_event, name='edit_event'),
     path('events/', views.event_list, name='event_list'),
+    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
     path('event/<int:event_id>/book/', views.book_event, name='book_event'),
 
     path('organizer/bookings/<int:event_id>/', views.organizer_manage_bookings, name='organizer_manage_bookings'),
